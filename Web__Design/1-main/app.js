8c92b8569b860521477a2a77e9fdff2ed6bdf737
Calvin <calvincurrie01@gmail.com>
2022-11-10 12:50:50 +1000

    E-Portfolio Dark Theme Set-Up

--- Web__Design/1-main/app.js
+++ Web__Design/1-main/app.js
@@ -8,3 +8,13 @@ function toggleModal() {
   document.body.classList += " modal--open";
 }
 
+let darkTheme = false;
+function toggleTheme() {
+  if (darkTheme) {
+    darkTheme = false;
+    console.log("helloworld")
+    return document.body.classList.remove("darkTheme");
+  }
+  darkTheme = true;
+  document.body.classList += "darkTheme";
+}
