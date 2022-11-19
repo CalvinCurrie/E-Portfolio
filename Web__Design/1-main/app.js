b3af3be4fe2453f334844c199904c48cbc572691
Calvin <calvincurrie01@gmail.com>
2022-11-19 13:47:44 +1000

    E-Portfolio--Landing__Page--Moving__Background

--- Web__Design/1-main/app.js
+++ Web__Design/1-main/app.js
@@ -12,9 +12,22 @@ let darkTheme = false;
 function toggleTheme() {
   if (darkTheme) {
     darkTheme = false;
-    console.log("helloworld")
+    console.log("helloworld");
     return document.body.classList.remove("darkTheme");
   }
   darkTheme = true;
   document.body.classList += "darkTheme";
+}
+
+const scaleFactor = 1 / 20;
+
+function moveBackground(event) {
+  const shapes = document.querySelectorAll(".shape");
+  const x = event.clientX * scaleFactor;
+  const y = event.clientY * scaleFactor;
+  for (let i = 0; i < shapes.length; ++i) {
+    const isOdd = i % 2 !== 0;
+    const boolInt = isOdd ? -1 : 1;
+    shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`
+  }
 }
